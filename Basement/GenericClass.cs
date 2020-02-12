2dd9e56b4c2043ab7e4e1888338c2ce39da7f5f5
pN <isuway@gmail.com>
2020-02-13 00:04:58 +0300

    [add] overload operator

--- Basement/GenericClass.cs
+++ Basement/GenericClass.cs
@@ -3,36 +3,8 @@ using System;
 
 namespace Basement
 {
-    public class Force<T1> : IProceed where T1 : class, new()
+    public class Force<T1> where T1 : class, new()
     {
-        public class Class1 : IDisposable
-        {
-            public void Dispose()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Doo()
-            {
-                Console.WriteLine("inner method");
-            }
-        }
-        public class Class2
-        {
-            public void DoIt()
-            {
-                using (var class1 = new Class1())
-                {
-                    class1.Doo();
-                }
-            }
-        }
-
-        public void Act()
-        {
-
-        }
-
         // class means reference type
         // new() means T1 must have default constructor
         public void WithYou<T2>(T2 t2) where T2 : ICloneable
@@ -42,5 +14,4 @@ namespace Basement
             T2 clone = (T2)t2.Clone();
         }
     }
-
 }
---
+++ Basement/OverloadOperator.cs
@@ -0,0 +1,41 @@
+﻿using Basement.Abstract;
+using System;
+
+namespace Basement
+{
+    class Distance
+    {
+        public int meter { get; set; }
+        public static bool operator <(Distance d1, Distance d2)
+        {
+            return (d1.meter < d2.meter);
+        }
+        public static bool operator >(Distance d1, Distance d2)
+        {
+            return (d1.meter > d2.meter);
+        }
+    }
+
+    public class OverloadOperator : IProceed
+    {
+        public void Act()
+        {
+            Run1();
+        }
+
+        void Run1()
+        {
+            Distance d1 = new Distance { meter = 10 };
+            Distance d2 = new Distance { meter = 20 };
+            if (d1 < d2)
+            {
+                Console.WriteLine("d1 is less than d2");
+            }
+            else if (d2 < d1)
+            {
+                Console.WriteLine("d2 is less than d1");
+            }
+        }
+    }
+
+}
