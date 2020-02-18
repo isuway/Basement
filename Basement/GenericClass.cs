dd248c64d226693cffee469596ad364ee95857d7
pN <isuway@gmail.com>
2020-02-18 23:12:31 +0300

    [master][add]

--- Basement/GenericClass.cs
+++ Basement/GenericClass.cs
@@ -3,6 +3,13 @@ using System;
 
 namespace Basement
 {
+    public class Bar
+    {
+        public override string ToString()
+        {
+            return "Bar class is working";
+        }
+    }
     public class Force<T1> where T1 : class, new()
     {
         // class means reference type
@@ -10,8 +17,29 @@ namespace Basement
         public void WithYou<T2>(T2 t2) where T2 : ICloneable
         {
             // T2 must implement interface IClonable
-            T1 item = new T1();
             T2 clone = (T2)t2.Clone();
+            Console.WriteLine(clone.ToString());
+        }
+
+        public void Print()
+        {
+            T1 item = new T1();
+            Console.WriteLine(item.ToString());
+        }
+    }
+    public class GenericClass : IProceed
+    {
+        public void Act()
+        {
+            Run1();
+        }
+        
+        private void Run1()
+        {
+            var barItem = new Force<Bar>();
+            barItem.Print();
+            var item = new Force<Random>();
+            item.WithYou<String>("GenericClass is working");
         }
     }
 }
--- Basement/Strings.cs
+++ Basement/Strings.cs
@@ -8,14 +8,22 @@ namespace Basement
     {
         public void Act()
         {
+            double x, y;
+            x = 0;
+            y = 0;
+            Console.WriteLine(x / y);
             Run1();
             Run2();
         }
 
         private void Run2()
         {
+#if DEBUG
             Console.WriteLine(DateTime.Now.ToString("D", new CultureInfo("en-En")));
             Console.WriteLine(DateTime.Now.ToString("d"));
+#else
+            Console.WriteLine("not debug");
+#endif
         }
 
         private void Run1()
