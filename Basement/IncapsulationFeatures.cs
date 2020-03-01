32de122d0b4272c5ad3d70bdd02e09a871e47e42
pN <isuway@gmail.com>
2020-03-01 21:31:27 +0300

    [add]

---
+++ Basement/IncapsulationFeatures.cs
@@ -0,0 +1,49 @@
+﻿using Basement.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basement
+{
+    public class IncapsulationFeatures : IProceed
+    {
+        public void Act()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Do1()
+        {
+
+        }
+
+        class Person
+        {
+            protected string FirstName
+            {
+                get;
+                set;
+            }
+        }
+        class Boss : Person
+        {
+            public void Foo()
+            {
+                FirstName = "name";
+                var name = FirstName;
+            }
+        }
+        class Employee
+        {
+            public void Foo()
+            {
+                var person = new Person();
+                //person.FirstName = "name";
+                //var name = person.FirstName;
+            }
+        }
+
+
+
+    }
+}
